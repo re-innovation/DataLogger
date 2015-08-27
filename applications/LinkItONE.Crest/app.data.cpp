f59c1f1ac5b8edd25787600d0c1d1af4deb2e19d
James Fowkes <jamesfowkes@gmail.com>
2015-08-27 17:39:23 +0100

    Fix for data manager issue (storing data in wrong fields)

--- applications/LinkItONE.Crest/app.data.cpp
+++ applications/LinkItONE.Crest/app.data.cpp
@@ -92,6 +92,7 @@ static void debugTaskFn(void)
 {
     uint8_t i;
     uint8_t fieldCount = s_dataDebugManager->fieldCount();
+    uint32_t * channelNumbers = s_dataDebugManager->getChannelNumbers();
 
     while(s_dataDebugManager->hasData())
     {
@@ -102,10 +103,13 @@ static void debugTaskFn(void)
             float toShow = ((NumericDataField *)s_dataDebugManager->getField(i))->getConvData(true);
              
             if (s_debugFieldFlags[i])
-            {   Serial.print(toShow);
+            {
+                Serial.print(toShow);
                 Serial.print("(");
                 Serial.print(average);
-                Serial.print(")");
+                Serial.print(")[");
+                Serial.print(channelNumbers[i]);
+                Serial.print("]");
                 Serial.print(", ");
             }
         }
